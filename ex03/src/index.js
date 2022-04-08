924b11efdad667a2281dbad54e9fe577a5f51550
unknown <aisa.smajic27@gmail.com>
2022-04-08 14:35:10 +0200

    zadatak 3

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,15 +1,18 @@
 // Only change code below this line
+
 function rangeOfNumbers(startN, endN) {
-    if (endN - startN === 0) {
+    if (startN === endN) {
       return [startN];
-    } 
-      var numbers = rangeOfNumbers(startN, endN - 1);
-      numbers.push(endN);
-      return numbers;
+    } else if (startN > endN) {
+      return "The starting number will always be less than or equal to the ending number";
+    }
+    return [startN].concat(rangeOfNumbers(startN + 1, endN));
   }
-// Only change code above this line
-console.log(rangeOfNumbers(1, 7)); // Change this line
-console.log(rangeOfNumbers(3, 10)); 
-console.log(rangeOfNumbers(5, 5)); 
-console.log(rangeOfNumbers(7, 5)); 
-module.exports = rangeOfNumbers;
+  
+  console.log(rangeOfNumbers(1, 7));
+  console.log(rangeOfNumbers(3, 10));
+  console.log(rangeOfNumbers(5, 5));
+  console.log(rangeOfNumbers(7, 5));
+  
+  // Only change code above this line
+  module.exports = rangeOfNumbers;
